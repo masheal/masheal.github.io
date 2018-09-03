04b0fccccd5a0539d7083952a5ee6cda06b66b2c
shengma <shengma>
2018-09-03 09:00:41 +0800

    change

--- sheng-webpack-simple/src/slide.vue
+++ sheng-webpack-simple/src/slide.vue
@@ -28,4 +28,9 @@ export default {
 <style>
 .slide{width: 490px;height: 290px;border: 1px solid black;position: relative;margin-left: 50%;transform: translateX(-50%);margin-top: 50px;overflow: hidden;}
 .slide li {width:230px;height:150px;position: absolute;left: 50%;transform: translateX(-50%);top: 25%;}
+.slide .s2 {z-index: 5;}
+.slide .s1 {transform: perspective(800px) translate(-75px) rotateY(30deg);z-index: 4;opacity: 0.2;}
+.slide .s0 {transform: perspective(800px) translate(-150px) rotateY(60deg);z-index: 3;opacity: 0.5;}
+.slide .s3 {transform: perspective(800px) translate(75px) rotateY(-30deg);z-index: 4;opacity: 0.5;}
+.slide .s4 {transform: perspective(800px) translate(150px) rotateY(-60deg);z-index: 3;opacity: 0.2;}
 </style>
