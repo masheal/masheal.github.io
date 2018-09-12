08094c56b0e09b27088dd94a6633936bc5d19f25
masheal <smzc7@mail.missouri.edu>
2018-09-12 22:20:04 +0800

    change

--- vueStudy/router-vue2.0/app.js
+++ vueStudy/router-vue2.0/app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const server = express();
 
 server.listen(1208);
-
 server.use('/main', (req, res) => {
-    // if (req.query.id == '1234') {
-    //     res.send({ data: 'One' })
-    // } else if (req.query.id == '5678') {
-    //     res.send({ data: 'Five' })
-    // }
-    console.log(req.query.id)
-})
+    console.log(req.query);
+    if (req.query.id == '1234') {
+        res.send({
+            ok: 1,
+            data: 'one'
+        })
+    } else if (req.query.id == '5678') {
+        res.send({
+            ok: 1,
+            data: 'five'
+        })
+    }
+});
 
 server.use(express.static('./'));
