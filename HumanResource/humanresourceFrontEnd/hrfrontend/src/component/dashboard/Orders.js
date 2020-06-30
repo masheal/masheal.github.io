997dd4993d6ffcdc394ff43b6ed37d04285a8723
Sheng Ma <shengma@Shengs-MacBook-Pro.local>
2020-06-30 15:56:02 -0400

    update

--- HumanResource/humanresourceFrontEnd/hrfrontend/src/component/dashboard/Orders.js
+++ HumanResource/humanresourceFrontEnd/hrfrontend/src/component/dashboard/Orders.js
@@ -9,50 +9,28 @@ import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
 
 // Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
+function createData(id, name, job, manager, hireDate, salary, deptId) {
+  return { id, name, job, manager, hireDate, salary, deptId };
 }
 
 const rows = [
   createData(
-    0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
+    7369,
+    'SMITH',
+    'CLERK',
+    7902,
+    '1980-12-17T00:00:00.000+0000',
+    800.0,
+    20,
   ),
   createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
-  createData(
-    2,
-    '16 Mar, 2019',
-    'Tom Scholz',
-    'Boston, MA',
-    'MC ⠀•••• 1253',
-    100.81,
-  ),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
+    7430,
+    'WAYNE',
+    'CLERK',
+    7698,
+    '1981-12-03T00:00:00.000+0000',
+    950.0,
+    70,
   ),
 ];
 
@@ -70,32 +48,36 @@ export default function Orders() {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <Title>Recent Orders</Title>
+      <Title>Employee Overview</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
+            <TableCell>Id</TableCell>
             <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Job</TableCell>
+            <TableCell>Manager</TableCell>
+            <TableCell>Hire Date</TableCell>
+            <TableCell>Salary</TableCell>
+            <TableCell align="right">Department Id</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
+              <TableCell>{row.id}</TableCell>
               <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
+              <TableCell>{row.job}</TableCell>
+              <TableCell>{row.manager}</TableCell>
+              <TableCell>{row.hireDate}</TableCell>
+              <TableCell>{row.salary}</TableCell>
+              <TableCell align="right">{row.deptId}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
       <div className={classes.seeMore}>
         <Link color="primary" href="#" onClick={preventDefault}>
-          See more orders
+          See more
         </Link>
       </div>
     </React.Fragment>
